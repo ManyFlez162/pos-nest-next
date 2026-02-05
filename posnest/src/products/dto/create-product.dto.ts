653ab2b259265a7a30b793392d376261c714a468
ManyFlez162 <yarvis42@gmail.com>
2026-02-05 16:21:39 -0700

    Products created

---
+++ posnest/src/products/dto/create-product.dto.ts
@@ -0,0 +1,19 @@
+import { IsInt, IsNotEmpty, IsNumber, IsString } from "class-validator"
+
+export class CreateProductDto {
+    @IsNotEmpty({ message: "El nombre del producto es obligatorio" })
+    @IsString({ message: "Nombre no válido" })
+    name: string
+
+    @IsNotEmpty({ message: "El precio del producto es obligatorio" })
+    @IsNumber({ maxDecimalPlaces: 2 }, { message: "Precio no válido" })
+    price: number
+
+    @IsNotEmpty({ message: "La cantidad no puede ir vacía" })
+    @IsNumber({ maxDecimalPlaces: 0 }, { message: "Cantidad no válida" })
+    inventory: number
+
+    @IsNotEmpty({ message: "La categoría no puede ir vacía" })
+    @IsInt({message: "La categoría no es válida"})
+    categoryId: number
+}
