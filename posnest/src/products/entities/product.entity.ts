784f5c35c6757bb643c7e8597a8159b007185207
ManyFlez162 <yarvis42@gmail.com>
2026-02-10 23:29:55 -0700

    products finished

--- posnest/src/products/entities/product.entity.ts
+++ posnest/src/products/entities/product.entity.ts
@@ -21,4 +21,7 @@ export class Product {
     @ManyToOne(() => Category)
     category: Category
 
+    @Column({type: 'int'})
+    categoryId: number
+
 }
--- posnest/src/products/products.service.ts
+++ posnest/src/products/products.service.ts
@@ -3,7 +3,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
-import { Repository } from 'typeorm';
+import { FindManyOptions, Repository } from 'typeorm';
 import { Category } from 'src/categories/entities/category.entity';
 
 @Injectable()
@@ -12,12 +12,13 @@ export class ProductsService {
     @InjectRepository(Product) private readonly productRepository: Repository<Product>,
     @InjectRepository(Category) private readonly categoryRepository: Repository<Category>
 
-  ) {}
+  ) { }
 
   async create(createProductDto: CreateProductDto) {
-    const category = await this.categoryRepository.findOneBy({id: createProductDto.categoryId})
-    if(!category) {
-      let errors : string[] = []
+    const category = await this.categoryRepository.findOneBy({ id: createProductDto.categoryId })
+
+    if (!category) {
+      let errors: string[] = []
       errors.push('La categoría no existe')
       throw new NotFoundException(errors)
     }
@@ -28,19 +29,74 @@ export class ProductsService {
     })
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(categoryId: number | null, take: number, skip: number) {
+    const options: FindManyOptions<Product> = {
+      relations: {
+        category: true
+      },
+      order: {
+        id: 'DESC'
+      },
+      take,
+      skip
+    }
+
+    if (categoryId) {
+      options.where = {
+        category: {
+          id: categoryId
+        }
+      }
+    }
+
+    const [products, total] = await this.productRepository.findAndCount(options)
+
+    return {
+      products,
+      total
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.productRepository.findOne({
+      where: {
+        id
+      },
+      relations: {
+        category: true
+      }
+    })
+
+    if (!product) {
+      throw new NotFoundException(`El producto con el ID: ${id} no fue encontrado`)
+    }
+
+    return product
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const product = await this.findOne(id)
+    Object.assign(product, updateProductDto)
+
+    if (updateProductDto.categoryId) {
+      const category = await this.categoryRepository.findOneBy({ id: updateProductDto.categoryId })
+
+      if (!category) {
+        let errors: string[] = []
+        errors.push('La categoría no existe')
+        throw new NotFoundException(errors)
+      }
+
+      product.category = category
+    }
+
+    return await this.productRepository.save(product)
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    const product = await this.findOne(id)
+    await this.productRepository.remove(product)
+    
+    return "Producto eliminado"
   }
 }
